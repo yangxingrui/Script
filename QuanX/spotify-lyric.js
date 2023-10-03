c126ace20023948fedb827608331d859c869761e
yangxingrui <95137682+yangxingrui@users.noreply.github.com>
2023-10-03 21:24:47 +0800

    Update spotify-lyric.js

--- QuanX/spotify-lyric.js
+++ QuanX/spotify-lyric.js
@@ -1,8 +1,4 @@
-const appid = '20231003001835922';
-const securityKey = 'tzwdq0I3GG0TnUhfssz5';
-
-$done({
-    response: {
-        body: $response.body.replace(/"access_token":\s*"[^"]+"/, `"access_token": "${appid}:${securityKey}"`)
-    }
-});
+const options = {
+    appid: '20231003001835922',
+    securityKey: 'tzwdq0I3GG0TnUhfssz5'
+};
